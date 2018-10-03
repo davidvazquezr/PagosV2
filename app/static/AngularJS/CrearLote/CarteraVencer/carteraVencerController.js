e15a22917389bc89e844d14208e948c124abbeb5
Laura <laura.gordillo.vargas@gmail.com>
2018-10-03 13:52:04 -0500

    Pasa variables

--- app/static/AngularJS/CrearLote/CarteraVencer/carteraVencerController.js
+++ app/static/AngularJS/CrearLote/CarteraVencer/carteraVencerController.js
@@ -1,5 +1,9 @@
 registrationModule.controller('carteraVencerController', function($scope, $rootScope, alertFactory) {
 	console.log('Entre al controller de la directiva CARTERA VENCER')
-    
+    console.log('Entre al controller de la directiva yuju')
+    $scope.$watch("customer.idEmpresa", function(newValue, oldValue) { 
+        console.log($scope.customer.idEmpresa)
+        console.log('SERA', $scope.customer)
+     },true);
     
 });
