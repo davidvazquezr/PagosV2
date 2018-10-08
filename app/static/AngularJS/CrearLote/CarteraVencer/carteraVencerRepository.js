a95d8333460cb8f6bb78b6f3c906daf3da9410bc
davidvazquezr <32405692+davidvazquezr@users.noreply.github.com>
2018-10-08 13:35:22 -0500

    xVencer

--- app/static/AngularJS/CrearLote/CarteraVencer/carteraVencerRepository.js
+++ app/static/AngularJS/CrearLote/CarteraVencer/carteraVencerRepository.js
@@ -4,10 +4,72 @@ registrationModule.factory('carteraVencerRepository', function($http) {
     return {
         getDatosxvencer: function(idEmpresa) {
             return $http({
-                url: crealLoteUrl + 'datosxvencer/',
+                url: carteraVencerUrl + 'datosxvencer/',
                 method: "GET",
                 params: {
                     idEmpresa: idEmpresa
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+          getDatosAprob: function(idLote) {
+            return $http({
+                url: carteraVencerUrl + 'datosAprob/',
+                method: "GET",
+                params: {
+                    idLote: idLote
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+          getOtrosIngresos: function(idLote) {
+            return $http({
+                url: carteraVencerUrl + 'otrosIngresos/',
+                method: "GET",
+                params: {
+                    idLote: idLote
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },        
+          getTransferencias: function(idLote) {
+            return $http({
+                url: carteraVencerUrl + 'transferencias/',
+                method: "GET",
+                params: {
+                    idLote: idLote
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },    
+          getIngresos: function(idEmpresa,idLote) {
+            return $http({
+                url: carteraVencerUrl + 'ingresos/',
+                method: "GET",
+                params: {
+                    idEmpresa:idEmpresa,
+                    idLote: idLote
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        },
+        getParametrosEscenarios: function(idEmpresa,idLote) {
+            return $http({
+                url: carteraVencerUrl + 'parametrosEscenarios/',
+                method: "GET",
+                params: {
+                    idEmpresa:idEmpresa,
+                    idLote: idLote
                 },
                 headers: {
                     'Content-Type': 'application/json'
